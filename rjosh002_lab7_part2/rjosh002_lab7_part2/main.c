a3efd96d6e112f951daa41a92513dd7a6f43673c
rjosh002 <rjosh002@ucr.edu>
2018-08-14 15:10:23 -0700

    lab 7 done

--- rjosh002_lab7_part2/rjosh002_lab7_part2/main.c
+++ rjosh002_lab7_part2/rjosh002_lab7_part2/main.c
@@ -8,12 +8,14 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "io.c"
-#define size 3
+#define size 4
 
 volatile unsigned char TimerFlag = 0;
 unsigned long _avr_timer_M = 1;
 unsigned long _avr_timer_cntcurr = 0;
+unsigned long tasksPeriod = 1;
 unsigned char flag = 0;
+unsigned char b4 = 0x10;
 unsigned char b3 = 0x08;
 unsigned char bottomB = 0x07;
 
@@ -28,52 +30,23 @@ task tasks[size];
 
 enum TL_states {TL_start, right, middle, left, wait} TL_state;
 int TickFct_ThreeLED(int state)
-{
-	static unsigned char count;
-	
+{	
 	switch(TL_state)
 	{ //transitions
 		case TL_start:
-		count = 0;
-		TL_state = right;
-		break;
-		case right:
-		bottomB = 0x01;
-		if(count<3){
-			count++;
-			TL_state=right;
-		}
-		else
-		{
-			count=0;
-			TL_state=middle;
-		}
-		break;
-		case middle:
-		bottomB = 0x02;
-		if(count<3)
-		{
-			count++;
-			TL_state=middle;
-		}
-		else
-		{
-			count=0;
-			TL_state = left;
-		}
-		break;
-		case left:
-		bottomB = 0x04;
-		if(count<3)
-		{
-			count++;
-			TL_state=left;
-		}
-		else
-		{
-			count=0;
 			TL_state = right;
-		}
+			break;
+		case right:
+			bottomB = 0x01;
+			TL_state = middle;
+			break;
+		case middle:
+			bottomB = 0x02;
+			TL_state = left;
+			break;
+		case left:
+			bottomB = 0x04;
+			TL_state = right;
 			break;
 		default:
 			TL_state = TL_start;
@@ -83,69 +56,68 @@ int TickFct_ThreeLED(int state)
 enum BL_states {BL_start, on, off} BL_state;
 int TickFct_BlinkLED(int state)
 {
-	static unsigned char count;
-	
 	switch(BL_state)
 	{ //transitions
 		case BL_start:
-		count = 0;
-		BL_state = on;
-		break;
-		case on:
-		b3 = 0x08;
-		if(count<10){
-			count++;
-			BL_state=on;
-		}
-		else{
-			count=0;
-			BL_state=off;
-		}
-		break;
-		case off:
-		b3 = 0x00;
-		if(count<10)
-		{
-			count++;
-			BL_state=off;
-		}
-		else
-		{
-			count=0;
 			BL_state = on;
-		}
-		break;
+			break;
+		case on:
+			b3 = 0x08;
+			BL_state = off;
+			break;
+		case off:
+			b3 = 0x00;
+			BL_state = on;
+			break;
 		default:
-		BL_state = BL_start;
-		break;
+			BL_state = BL_start;
+			break;
 	}
 };
+enum s_states {s_start, s_on, s_off} s_state;
+int TickFct_SState(int state)
+{
+	const unsigned char A0 = ~PINA & 0x01;
+	
+	switch(s_state)
+	{ //transitions
+		case s_start:
+			s_state= s_on;
+			break;
+		case s_on:
+			if (A0)
+			{
+				 b4 = 0x10;
+			}
+			else
+			{
+				b4 = 0x00;
+			}
+			s_state = s_off;
+			break;
+		case s_off:
+			b4 = 0x00;
+			s_state = s_on;
+			break;
+		default:
+			s_state = s_start;
+		break;
+	}
+}
 enum m_states {m_start, synch} m_state;
 int TickFct_mergedState(int state)
-{
-	static unsigned char count;
-	
+{	
 	switch(m_state)
 	{ //transitions
 		case m_start:
-			count=0;
 			m_state= synch;
 			break;
 		case synch:
-			if(count < 3) 
-			{
-				count++;
-				m_state = synch;
-			}
-			else // if(!(count < 10))
-			{
-				count=0;
-				PORTB = b3 | bottomB;
-				m_state = synch;
-			}
+
+			PORTB = b3 | bottomB | b4;
+			m_state = synch;
 			break;
 		default:	
-			count = 0;
 			m_state = m_start;
 			break;
 	}
@@ -172,7 +144,7 @@ void TimerISR() {
 			tasks[i].state = tasks[i].TickFct(tasks[i].state);
 			tasks[i].elapsedTime = 0;
 		}
-		tasks[i].elapsedTime += tasks[i].period;
+		tasks[i].elapsedTime += tasksPeriod;
 	}
 }
 
@@ -196,22 +168,27 @@ int main(void)
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00;
 
-	TimerSet(100);
+	TimerSet(tasksPeriod);
 	TimerOn();
 	
 	unsigned char i = 0;
 	tasks[i].state = BL_start;
-	tasks[i].period = 100;
+	tasks[i].period = 1000;
 	tasks[i].elapsedTime = 0;
 	tasks[i].TickFct = &TickFct_BlinkLED;
 	i++;
 	tasks[i].state = TL_start;
-	tasks[i].period = 100;
+	tasks[i].period = 300;
 	tasks[i].elapsedTime = 0;
 	tasks[i].TickFct = &TickFct_ThreeLED;
 	i++;
+	tasks[i].state = s_start;
+	tasks[i].period = 2;
+	tasks[i].elapsedTime = 0;
+	tasks[i].TickFct = &TickFct_SState;
+	i++;
 	tasks[i].state = m_start;
-	tasks[i].period = 100;
+	tasks[i].period = 1;
 	tasks[i].elapsedTime = 0;
 	tasks[i].TickFct = &TickFct_mergedState;
 	
